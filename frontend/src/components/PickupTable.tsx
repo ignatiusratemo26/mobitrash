0554c06e03e0d842aaa6d34b8ae2a8f4418a107f
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-08-01 15:39:11 +0300

    implemented the effectHook for fetching pickup requests

---
+++ frontend/src/components/PickupTable.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Table,Thead,Tbody,Tfoot,Tr,Th,Td,TableCaption,TableContainer,} from '@chakra-ui/react'
+import apiClient from '../services/api-client'
+import { CanceledError } from "axios"
+
+const PickupTable = () => {
+  return (
+
+    <TableContainer>
+        <Table variant='striped' colorScheme='teal' size='sm'>
+            <TableCaption>A summary of your recent pickup requests</TableCaption>
+            <Thead>
+            <Tr>
+                <Th>Pickup ID</Th>
+                <Th>Date</Th>
+                <Th>Actions</Th>
+                <Th isNumeric>Cost</Th>
+            </Tr>
+            </Thead>
+            <Tbody>
+            <Tr>
+                <Td>123#</Td>
+                <Td>DD/MM/YYYY TIME:TIME</Td>
+                <Td>Delete      view</Td>
+                <Td isNumeric>25.4</Td>
+            </Tr>
+            </Tbody>
+            <Tfoot>
+            <Tr>
+                <Th></Th>
+                <Th></Th>
+                <Th>TOTAL:</Th>
+                <Th isNumeric>Total ---here</Th>
+            </Tr>
+            </Tfoot>
+        </Table>
+    </TableContainer>
+
+  )
+}
+
+export default PickupTable
