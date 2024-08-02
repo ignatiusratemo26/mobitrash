fcefcf501d5e9974435ac4127bfef3b45d077273
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-08-02 19:43:08 +0300

    updated admin viewsets to allow patches and deletions of pickup requests

--- mobitrash/pickup/urls.py
+++ mobitrash/pickup/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import PickupRequestViewSet, AdminPickupRequestList
+from .views import PickupRequestViewSet, AdminPickupRequestViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pickup-requests', PickupRequestViewSet, basename='pickup-request')
@@ -8,8 +8,11 @@ router.register(r'pickup-requests', PickupRequestViewSet, basename='pickup-reque
 urlpatterns = [
     path('', include(router.urls)),
     path('recent-pickups/', PickupRequestViewSet.as_view({'get': 'recent_pickups'}), name='recent-pickups'),
-    path('admin/pickup-list/<int:pk>/update-weight/', PickupRequestViewSet.as_view({'put': 'update_weight'}), name='update-pickup-weight'),
-    path('admin/pickup-list/', AdminPickupRequestList.as_view(), name='admin-pickup-list'),
+    
+    path('admin/pickup-list/', AdminPickupRequestViewSet.as_view({'get':'list'}), name='admin-pickup-list'),
+    path('admin/pickup-list/<int:pk>/', AdminPickupRequestViewSet.as_view({'get': 'pickup_request','patch':'pickup_request', 'delete':'pickup_request'}), name='pickup-request'),
+    path('admin/pickup-list/<int:pk>/update-weight/', AdminPickupRequestViewSet.as_view({'patch': 'update_weight'}), name='update-pickup-weight'),
+    
     path('user/<int:user_id>/pickup-requests/', PickupRequestViewSet.as_view({'get': 'list_user_pickups'}), name='user-pickup-requests'),
     path('user/<int:user_id>/pickup-requests/<int:pk>/', PickupRequestViewSet.as_view({'get': 'retrieve_by_user'}), name='user-retrieve-pickup-request'),
 ]
--- mobitrash/pickup/views.py
+++ mobitrash/pickup/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth import get_user_model, authenticate
 from .permissions import IsOwnerOrReadOnly
 from .serializers import PickupRequestSerializer
@@ -12,7 +11,7 @@ from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 
 
-class AdminPickupRequestList(generics.ListCreateAPIView):
+class AdminPickupRequestViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated , permissions.IsAdminUser)
     serializer_class = PickupRequestSerializer
     queryset = PickupRequest.objects.all()
@@ -21,21 +20,38 @@ class AdminPickupRequestList(generics.ListCreateAPIView):
     ordering_fields = ['pickup_date', 'user__email','user__id', 'status', ]
     search_fields = ['pickup_date', 'user__email','user__id', 'status', ]  # Add fields you want to search on
     filterset_fields = ['pickup_date', 'user__email','user__id', 'status', ]
-
     
-    @action(detail=True, methods=['get'],  url_path='recent-pickups')
-    def recent_pickups(self, request):
-        recent_pickups = PickupRequest.objects.all().order_by('-pickup_date')
-
-        page = self.paginate_queryset(recent_pickups)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(recent_pickups, many=True)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+    @action(detail=True, methods=['get','patch','put', 'post'], url_path='pickup-request')
+    def pickup_request(self, request, pk=None):
+        pickup_request =self.get_object()
+        serializer = self.get_serializer(pickup_request)
         return Response(serializer.data)
     
+    @action(detail=True, methods=['get','patch'], url_path='update-weight')
+    def update_weight(self, request, pk=None):
+        pickup_request = self.get_object()
+        weight = request.data.get('weight')
+        pickup_request.weight = weight
+        pickup_request.calculate_amount_due()
+        pickup_request.status = 'In Progress'
+        pickup_request.save()
+        serializer = self.get_serializer(pickup_request)
+        return Response(serializer.data)
     
+    @action(detail=False, methods=['get'], url_path='user/(?P<user_id>\d+)/pickup-requests/(?P<pk>\d+)', url_name='user_retrieve_pickup_request')
+    def retrieve_by_user(self, request, user_id=None, pk=None):
+        try:
+            pickup_request = PickupRequest.objects.get(pk=pk, user_id=user_id)
+        except PickupRequest.DoesNotExist:
+            return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.get_serializer(pickup_request)
+        return Response(serializer.data)
+    
+
     
 class PickupRequestViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated ,IsOwnerOrReadOnly)
@@ -54,16 +70,7 @@ class PickupRequestViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
     
-    @action(detail=True, methods=[ 'put'], permission_classes=[permissions.IsAdminUser], url_path='update-weight')
-    def update_weight(self, request, pk=None):
-        pickup_request = self.get_object()
-        weight = request.data.get('weight')
-        pickup_request.weight = weight
-        pickup_request.calculate_amount_due()
-        pickup_request.status = 'In Progress'
-        pickup_request.save()
-        serializer = self.get_serializer(pickup_request)
-        return Response(serializer.data)
+    
     
     def recent_pickups(self, request):
         recent_pickups = PickupRequest.objects.filter(user=self.request.user).order_by('-pickup_date')
