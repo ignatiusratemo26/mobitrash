6dcc2f53fdd38a307ac925775f189356f9b2f763
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-08-10 13:13:28 +0300

    fixed the user data from api response

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,20 @@
-import { Box, Flex, Image, Input, AvatarGroup, Avatar, Text } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Box, Flex, Image, Input, AvatarGroup, Avatar, Text, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/react.svg';
 import ColorModeSwitch from './ColorModeSwitch';
 import useAuth from '../hooks/useAuth';
 
 const NavBar = () => {
   const { currentUser, logout } = useAuth();
-  
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate('/login'); // Redirect to login page after logout
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
 
   return (
     <Box as="header" bg="blue.500" p={3}>
@@ -17,9 +25,11 @@ const NavBar = () => {
         <ColorModeSwitch />
         
         <AvatarGroup spacing='1rem'>
-          <Avatar bg='grey.500' name='Iggy Ratemo' />
-          <Avatar bg='teal.500' />
+          <Avatar bg='grey.500' name={`${currentUser?.first_name} ${currentUser?.last_name}`}  />
         </AvatarGroup>
+        <Button colorScheme="red" variant="outline" onClick={handleLogout}>
+          Logout
+        </Button>
       </Flex>
     </Box>
   );
