60ed92035db9cfbcf164b1225c9305a34f3f9f00
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-08-10 16:17:46 +0300

    User profile page

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,28 +1,30 @@
-import { Box, Flex, Image, Input, AvatarGroup, Avatar, Text, Button, Menu, MenuList, MenuItem, MenuButton, Icon, useColorMode, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { Box, Flex, Image, Input, AvatarGroup, Avatar,
+   Menu, MenuList, MenuItem, MenuButton, Icon, 
+   useColorMode, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
-import logo from '../assets/react.svg';
-import ColorModeSwitch from './ColorModeSwitch';
+import logo from '../assets/images/LOGO.png';
 import useAuth from '../hooks/useAuth';
 import { AddIcon, SearchIcon } from '@chakra-ui/icons';
 import { FaCalendar, FaMoon, FaSignOutAlt, FaSun, FaUser } from 'react-icons/fa';
+import useLogout from '../hooks/useLogout';
 
 const NavBar = () => {
-  const { currentUser, logout } = useAuth();
+  const { currentUser } = useAuth();
   const navigate = useNavigate();
   const { colorMode, toggleColorMode } = useColorMode();
-  const handleLogout = async () => {
-    try {
-      await logout();
-      navigate('/login'); // Redirect to login page after logout
-    } catch (error) {
-      console.error('Logout failed:', error);
-    }
-  };
-
+  const handleLogout= useLogout();
   return (
     <Box as="header" bg="blue.500" p={3}>
       <Flex justify="space-between" align="center">
-        <Image src={logo} boxSize='60px' />
+      
+        <Image 
+          src={logo} 
+          boxSize="auto" // Set boxSize to auto to allow responsive sizing
+          maxWidth="100px" // You can adjust maxWidth as needed
+          height="auto" 
+          objectFit="contain" // Maintains aspect ratio
+        />
+
 
         <InputGroup width="300px">
           <InputLeftElement
