6386bc4f2098ecc7590bb716b249cf784638b5f1
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-08-10 17:01:28 +0300

    fixed request status badge and view button

--- frontend/src/components/PickupTable.tsx
+++ frontend/src/components/PickupTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer, Badge, Button } from '@chakra-ui/react';
 import useRequests from '../hooks/useRequests';
+import { ViewIcon } from '@chakra-ui/icons';
 
 const PickupTable = () => {
   const { requests, error, isLoading } = useRequests();
@@ -14,6 +15,7 @@ const PickupTable = () => {
           <Tr>
             <Th>Pickup ID</Th>
             <Th>Date</Th>
+            <Th>Status</Th>
             <Th>Actions</Th>
             <Th isNumeric>Cost</Th>
           </Tr>
@@ -23,7 +25,17 @@ const PickupTable = () => {
             <Tr key={request.id}>
               <Td>{request.id}</Td>
               <Td>{request.request_date}</Td>
-              <Td>Delete view</Td>
+              <Td><Badge
+                variant={request.status === 'Pending' ? 'outline' : 'solid'}
+                colorScheme={request.status === 'Completed' ? 'green' : 'yellow'}>
+                {request.status}
+              </Badge>
+              </Td>
+              <Td>
+              <Button size="xs" rightIcon={<ViewIcon />} colorScheme='teal' variant='outline' onClick={() => console.log(request.id)}>
+                View
+              </Button>
+              </Td>
               <Td isNumeric>{request.amount_due}</Td>
             </Tr>
           ))}
--- frontend/src/hooks/useRequests.ts
+++ frontend/src/hooks/useRequests.ts
@@ -7,6 +7,7 @@ interface Request {
     user_email: string;
     request_date: string;
     amount_due: number;
+    status: string;
 
 }
 interface FetchRequestsResponse {
