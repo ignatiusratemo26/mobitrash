0512a0eb04775f4f5b40321044724674e06fd8fe
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-08-24 16:13:31 +0300

    created the payment serializer and viewsets

--- mobitrash/payments/views.py
+++ mobitrash/payments/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Payment
+from .serializers import PaymentSerializer
 
-# Create your views here.
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('user', 'status')
+    search_fields = ('user__username', 'user__email', 'status')
+    
+    def get_queryset(self):
+        return Payment.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
